b5000fb18f9d2f08eafb908d00473bbc0d147ce6
SunF0x <57665335+SunF0x@users.noreply.github.com>
2023-10-09 22:35:26 +0300

    Remove comments in SerialDAO.java

--- J2EE/src/main/java/org/example/SerialDAO.java
+++ J2EE/src/main/java/org/example/SerialDAO.java
@@ -14,7 +14,6 @@ public class SerialDAO {
     public List<Serial> getSerial() {
         List<Serial> serials = new ArrayList<>();
         try ( Statement stmt = connection.createStatement();){
-            //Statement stmt = connection.createStatement();
             ResultSet rs = stmt.executeQuery("select * from serials;");
             while (rs.next()) {
                 String title = rs.getString("title");
@@ -32,28 +31,4 @@ public class SerialDAO {
         System.out.println(serials);
         return serials;
     }
-//    public List<Serial> getSerialByName(String title) {
-//        List<Serial> serials = new ArrayList();
-//        //try (Connection connection = ConnectionUtil.getConnection()) {
-//        try (Statement stmt = connection.createStatement()) {
-//            //Statement stmt = connection.createStatement();
-//            ResultSet rs = stmt.executeQuery("select * from serials;");
-//            while (rs.next()) {
-//                String title1 = rs.getString("title");
-//                String character = rs.getString("character");
-//                int seasons = rs.getInt("seasons");
-//                int episodes = rs.getInt("episodes");
-//                Date year = rs.getDate("year");
-//                Serial serial = new Serial(title, character, seasons, episodes, year);
-//                if (Objects.equals(title1, title)) {
-//                    serials.add(serial);
-//                }
-//            }
-//        } catch (SQLException ex) {
-//            Logger.getLogger(PostgreSQLDAO.class.getName()).log(Level.SEVERE,
-//                    null, ex);
-//        }
-//        System.out.println(serials);
-//        return serials;
-//    }
 }
